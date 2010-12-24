e76bc7ea865d5a67ce0bd941dd836aed408de934
Duncan Gough <duncan@suttree.com>
2010-12-24 09:06:01 +0000

    first commit from ryan

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GiveGoodSunflower::Application.config.secret_token = 'ee2214c50c4fc67cf62bc8353adfd50a79b9615f2534eea5630560bc9e79f38fba42150cc144e674667a7de1a1d90ffb4cb580fb29de07bed2dc1d5bc000b684'
