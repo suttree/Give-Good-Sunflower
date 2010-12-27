11c4ca490f0bbc78c065f42f6c63520b22590f49
Duncan Gough <duncan@suttree.com>
2010-12-27 17:22:50 +0000

    before filter to setup the twitter gem if we're logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :setup_twitter
+
+  protected
+  def setup_twitter
+    if current_user
+      Twitter.configure do |config|
+        config.consumer_key = Omnisocial.service_configs[:twitter].app_key
+        config.consumer_secret = Omnisocial.service_configs[:twitter].app_secret
+        config.oauth_token = current_user.login_account.token
+        config.oauth_token_secret = current_user.login_account.secret
+      end
+    end
+  end
 end
