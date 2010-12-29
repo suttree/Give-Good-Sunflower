dd96447cccc1c63374495928b678705f56d5855c
Duncan Gough <duncan@suttree.com>
2010-12-29 18:07:18 +0000

    Missing drop table for email_addresses

--- db/migrate/20101227165229_create_omnisocial_tables.rb
+++ db/migrate/20101227165229_create_omnisocial_tables.rb
@@ -3,10 +3,10 @@ class CreateOmnisocialTables < ActiveRecord::Migration
     create_table :users do |t|
       t.string :remember_token
       # Any additional fields here
-      
+
       t.timestamps
     end
-    
+
     create_table :login_accounts do |t|
       t.string :type
       t.integer :user_id
@@ -14,11 +14,11 @@ class CreateOmnisocialTables < ActiveRecord::Migration
       t.string :name
       t.string :login
       t.string :picture_url
-      # Any additional fields here 
+      # Any additional fields here
       t.string :token
       t.string :secret
       t.text :user_hash
-      
+
       t.timestamps
     end
     create_table :email_addresses do |t|
@@ -27,7 +27,7 @@ class CreateOmnisocialTables < ActiveRecord::Migration
 
       t.timestamps
     end
-    
+
     add_index :login_accounts, :user_id
     add_index :login_accounts, :type
     add_index :login_accounts, [:type, :remote_account_id], :name => 'type_remote_acc_id'
@@ -37,6 +37,7 @@ class CreateOmnisocialTables < ActiveRecord::Migration
     remove_index :login_accounts, :name => 'type_remote_acc_id'
     remove_index :login_accounts, :type
     remove_index :login_accounts, :user_id
+    drop_table :email_addresses
     drop_table :login_accounts
     drop_table :users
   end
