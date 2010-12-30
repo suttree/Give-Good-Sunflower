37a33058928e2141d5eab68d22e10773e80c8f3d
Duncan Gough <duncan@suttree.com>
2010-12-30 11:18:35 +0000

    Styling

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
   before_filter :require_user, :only => :unread
 
   def unread
+    @title_prefix = "#{current_user.login} gave good sunflower"
     @articles = current_user.articles.paginate :page => params[:page], :per_page => 1
   end
 end
