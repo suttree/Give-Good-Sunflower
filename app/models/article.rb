d4b45acd74a4aaae54efeb24de0b787fcf4d6829
Duncan Gough <duncan@suttree.com>
2010-12-31 22:38:00 +0000

    Validating the presence of some fields

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,6 +4,7 @@ class Article < ActiveRecord::Base
   serialize :keywords
 
   validates_uniqueness_of :tweet_id
+  validates_presence_of :body, :html_body, :title, :url
 
   default_scope :order => 'created_at DESC'
 end
