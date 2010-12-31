20aafd524f73ed0cf6e1a35bd134945a69bb58a7
Duncan Gough <duncan@suttree.com>
2010-12-31 22:38:04 +0000

    Schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101228221254) do
+ActiveRecord::Schema.define(:version => 20101231211047) do
 
   create_table "articles", :force => true do |t|
     t.integer  "user_id",                          :null => false
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(:version => 20101228221254) do
     t.string   "remember_token"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "last_tweet_id",  :limit => 8
   end
 
 end
