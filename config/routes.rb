242bcdaa2132439a55f82d99ddfcd4d6125a94ed
Duncan Gough <duncan@suttree.com>
2011-01-01 22:45:40 +0000

    Working permalinks ;-)

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 GiveGoodSunflower::Application.routes.draw do
   root :to => "home#index"
 
-  match "/unread(/:page)", :to => "home#unread"
-  match "/archives(/:page)", :to => "home#archives"
-  match "/read/:id", :to => "home#read"
+  match "/unread(/:page)", :to => "home#unread", :as => 'unread'
+  match "/archives(/:page)", :to => "home#archives", :as => 'archives'
+  match "/read/:id", :to => "home#read", :as => 'read'
   match "/cron", :to => "home#cron"
 end
